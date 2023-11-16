f5e36b640ff49713d311ded1e18c1f6560b77cb9
Thomas Wallsmith <thomaswallsmith@gmail.com>
2023-11-15 21:25:27 -0500

    feat: added INSANE background animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,36 @@
 <slot />
 
 <style>
+	@property --a {
+		syntax: '<length>';
+		inherits: false;
+		initial-value: 0px;
+	}
+	@property --b {
+		syntax: '<length>';
+		inherits: false;
+		initial-value: 0px;
+	}
+
 	:global(body) {
 		background: black;
+		animation-name: move;
 		background-image: radial-gradient(white 1px, transparent 0);
 		background-size: 40px 40px;
+		background-position: var(--a) var(--a);
+		animation-direction: alternate-reverse;
+		animation-iteration-count: infinite;
+		animation-duration: 10s;
+	}
+
+	@keyframes move {
+		from {
+			--a: 0px;
+			--b: 0px;
+		}
+		to {
+			--a: 1000px;
+			--b: 1000px;
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
-<div class="flex flex-col mt-5 mx-5 h-[100%] justify-center align-middle">
-	<h1 class="text-4xl mr-auto">thomaswallsmith.com</h1>
-	<p class="mr-auto">The fullstack developer of your dreams.</p>
-	<div class="flex flex-col justify-center mt-10">
-		<p class="text-lg mx-auto">This website is currently under construction</p>
-		<p class="mx-auto">If you'd like to reach me, email me at thomaswallsmith@gmail.com</p>
+<div class="flex flex-col mt-20 h-[100%] justify-center align-middle">
+	<div class="mx-auto text-center">
+		<h1 class="text-4xl mr-auto"><span>thomaswallsmith.com</span></h1>
+		<p class=""><span>the fullstack developer of your dreams</span></p>
+	</div>
+	<div class="flex flex-col mt-5 mx-3 text-center">
+		<p class="">
+			<span> This website is currently under construction </span>
+		</p>
+		<p class="">
+			<span> If you'd like to reach me, email me at thomaswallsmith@gmail.com </span>
+		</p>
 	</div>
 </div>
 
@@ -13,6 +19,10 @@
 	}
 	h1,
 	p {
+		@apply font-mono;
+	}
+	span {
 		@apply bg-black;
+		@apply p-1;
 	}
 </style>
