86bd721eb81d307cb4b1d3da2d9a68d33d387e73
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-19 19:39:22 +0000

    fix: font sizing & refactors

---
+++ src/lib/components/SiteHeader.svelte
@@ -0,0 +1,29 @@
+<div class="mx-auto text-center">
+	<h1 class="box text-3xl mr-auto"><span>thomaswallsmith.com</span></h1>
+	<p class="text-sm"><span>the fullstack developer of your dreams</span></p>
+</div>
+
+<style lang="postcss">
+	@keyframes cursor-blink {
+		0% {
+			opacity: 0;
+		}
+	}
+	.box::after {
+		translate: 0 5px;
+		margin-left: 2px;
+		content: '';
+		width: 3px;
+		height: 1em;
+		background: #ffffff;
+		display: inline-block;
+		animation: cursor-blink 1.5s steps(2) infinite;
+	}
+	h1,
+	p {
+		@apply font-mono;
+	}
+	span {
+		@apply bg-black;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
+<script lang="ts">
+	import SiteHeader from "$lib/components/SiteHeader.svelte";
+</script>
+
 <div class="flex flex-col mt-20 h-[100%] justify-center align-middle">
-	<div class="mx-auto text-center">
-		<h1 class="box text-4xl mr-auto"><span>thomaswallsmith.com</span></h1>
-		<p class=""><span>the fullstack developer of your dreams</span></p>
-	</div>
+	<SiteHeader />
 	<div class="flex flex-col mt-5 mx-10 text-center">
 		<p class="">
 			<span> This website is currently under construction </span>
@@ -17,26 +18,10 @@
 	* {
 		@apply text-white;
 	}
-	h1,
 	p {
 		@apply font-mono;
 	}
 	span {
 		@apply bg-black;
-	}
-	@keyframes cursor-blink {
-		0% {
-			opacity: 0;
-		}
-	}
-	.box::after {
-		translate: 0 5px;
-		margin-left: 2px;
-		content: '';
-		width: 3px;
-		height: 35px;
-		background: #ffffff;
-		display: inline-block;
-		animation: cursor-blink 1.5s steps(2) infinite;
 	}
 </style>
