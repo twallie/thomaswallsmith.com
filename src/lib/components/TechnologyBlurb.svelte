ac969845f6eb34bd93d6d1d9201ac2d24471917a
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-22 06:50:25 +0000

    feat: technologies page content complete

---
+++ src/lib/components/TechnologyBlurb.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { Bulletpoint } from '$lib/types';
+	import Collapsible from './Collapsible.svelte';
+	import { fly } from 'svelte/transition';
+
+	export let title: string;
+	export let description: string;
+	export let bulletpoints: Bulletpoint[];
+
+	export let opened = false;
+</script>
+
+<Collapsible {title} bind:opened />
+{#if opened}
+	<div
+		in:fly={{ x: -200, duration: 200 }}
+		out:fly={{ x: 200, duration: 200 }}
+		class="mb-5 ml-2 mr-3"
+	>
+		<p class="text-right">
+			<span>
+				{description}
+			</span>
+		</p>
+		<div class="flex flex-row flex-wrap justify-around xl:ml-[50%] space-x-3 mt-4 mb-4">
+			{#each bulletpoints as bulletpoint}
+				<div class="mt-2">
+					<span class="text-xl underline font-bold">{bulletpoint.title}</span>
+					<ul>
+						{#each bulletpoint.points as point}
+							<li>
+								<span> - {point}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
+
+<style lang="postcss">
+	span {
+		@apply bg-black;
+	}
+</style>
--- src/routes/technologies/+page.svelte
+++ src/routes/technologies/+page.svelte
@@ -1,14 +1,71 @@
 <script lang="ts">
 	import Collapsible from '$lib/components/Collapsible.svelte';
 	import SubsectionHeader from '$lib/components/SubsectionHeader.svelte';
-	import JavaScriptTypeScript from '$lib/components/technologies/JavaScriptTypeScript.svelte';
-	import Triangle from '$lib/images/triangle.svg';
+	import TechnologyBlurb from '$lib/components/TechnologyBlurb.svelte';
+
+	import type { Bulletpoint } from '$lib/types';
+
+	const jsTsBulletpoints: Bulletpoint[] = [
+		{
+			title: 'Runtimes',
+			points: ['Node.js', 'Bun', 'V8']
+		},
+		{
+			title: 'Frontend',
+			points: ['Svelte', 'React']
+		},
+		{
+			title: 'Backend',
+			points: ['SvelteKit', 'Express.js', 'Next.js']
+		},
+		{
+			title: 'APIs/SDKs',
+			points: ['OpenAI JavaScript API', 'Firebase JavaScript SDK', 'supabase-js']
+		}
+	];
+
+	const pythonBulletpoints: Bulletpoint[] = [
+		{
+			title: 'WebDev',
+			points: ['Django', 'Flask']
+		},
+		{
+			title: 'APIs/SDKs',
+			points: ['OpenAI Python API', 'Firebase Python SDK']
+		}
+	];
+
+	const dbBulletpoints: Bulletpoint[] = [
+		{
+			title: 'Relational',
+			points: ['MySQL', 'PostgreSQL']
+		},
+		{
+			title: 'NoSQL',
+			points: ['MongoDB', 'Firestore']
+		},
+		{
+			title: 'BaaS',
+			points: ['Supabase', 'Firebase']
+		}
+	];
 </script>
 
 <SubsectionHeader pageTitle="technologies" />
 <div class="flex flex-col mt-4 font-mono">
-	<JavaScriptTypeScript />
-	<Collapsible title="Python" />
-	<Collapsible title="Databases" />
-	<Collapsible title="Java" />
+	<TechnologyBlurb
+		title="JavaScript & TypeScript"
+		description="TypeScript and it's subset JavaScript are my preferred languages for all things web dev."
+		bulletpoints={jsTsBulletpoints}
+	/>
+	<TechnologyBlurb
+		title="Python"
+		description="Proficient in Python, leveraging its versatility and readability to develop robust web applications."
+		bulletpoints={pythonBulletpoints}
+	/>
+	<TechnologyBlurb
+		title="Databases"
+		description="Proficient in SQL and NoSQL databases, I design efficient solutions for seamless data management and retrieval, ensuring optimal application interaction."
+		bulletpoints={dbBulletpoints}
+	/>
 </div>
