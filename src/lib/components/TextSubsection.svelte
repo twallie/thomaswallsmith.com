5a6c0da1ae3a36b1362fc3cc9b05c35e3d876197
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-12-01 05:36:54 +0000

    fix: spacing for text subsections

---
+++ src/lib/components/TextSubsection.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import SubsectionHeader from '$lib/components/SubsectionHeader.svelte';
+	export let pageTitle: string;
+	export let content: string;
+</script>
+
+<SubsectionHeader {pageTitle} />
+<div class="flex flex-col ml-[18%] text-sm sm:text-lg xl:ml-[50%]">
+	<p class="text-end font-mono">
+		<span>
+			{content}
+		</span>
+	</p>
+</div>
