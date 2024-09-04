6a516d72b608d2ca9c75d74a05f808ce99763797
twallie <thomaswallsmith@gmail.com>
2024-09-04 12:16:16 -0400

    build: add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mx-10 mt-5">
+	<h1 class="text-2xl">thomaswallsmith.com</h1>
+	<p>Under construction...</p>
+</div>
